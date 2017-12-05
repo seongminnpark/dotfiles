46514e629930f231092d3f10bd9609c3a7944d40
Seongmin Park <seongminnpark@gmail.com>
2017-12-05 11:04:51 -0500

    Remove restrictions

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -13,18 +13,12 @@ dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
 
 # Suspend dunst and lock, then resume dunst when unlocked.
 pkill -u $USER -USR1 dunst
-# i3lock -n -i $HOME/wallpaper.png \
-#     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
-#     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
-#     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-#     --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
-#     --radius=15 --veriftext="" --wrongtext=""
 i3lock -n -i $HOME/wallpaper.png \
-    --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
-    --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
-    --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-    --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
-    --radius=15 --veriftext="" --wrongtext=""
+     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
+     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
+     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
+     --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
+     --radius=15 --veriftext="" --wrongtext=""
 
 pkill -u $USER -USR2 dunst
 
