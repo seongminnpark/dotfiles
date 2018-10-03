dc3e8a33bd7e02a9f01107520bf0069ec76a7ddb
Seongmin Park <seongminnpark@gmail.com>
2018-10-02 23:26:57 -0400

    Change launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar -c $HOME/.config/polybar/config top &
+polybar -c $HOME/.config/polybar/config main &
 
